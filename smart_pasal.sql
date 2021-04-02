094e90b806e525c57252ff3c3148d74999faf540
Sagar Devkota <sagardevkota55@gmail.com>
2021-04-02 15:34:08 +0545

    added properties and profiles for dev and prod env

--- smart_pasal.sql
+++ smart_pasal.sql
@@ -3,13 +3,15 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 02, 2021 at 10:43 AM
+-- Generation Time: Apr 02, 2021 at 10:54 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
